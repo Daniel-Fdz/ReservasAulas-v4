e02a6e874d278f81df38c3cde8c5f27b0bdd1448
Daniel-Fdz <93385389+Daniel-Fdz@users.noreply.github.com>
2022-03-09 00:16:47 +0100

    Modificación y finalización de la clase Permanencia

---
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/dominio/Permanencia.java
@@ -0,0 +1,50 @@
+package org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public abstract class Permanencia {
+
+	private LocalDate dia;
+	protected static final DateTimeFormatter FORMATO_DIA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
+	// Constructor que acepta un día y un tramo como parámetros
+	public Permanencia(LocalDate dia) {
+		setDia(dia);
+	}
+	
+	// Constructor copia
+	public Permanencia(Permanencia permanencia) {
+		if(permanencia == null) {
+			throw new NullPointerException("ERROR: No se puede copiar una permanencia nula.");
+		} else {
+			setDia(permanencia.getDia());
+		}
+	}
+
+	// Devuelve el día
+	public LocalDate getDia() {
+		return dia;
+	}
+
+	// Establece el día
+	private void setDia(LocalDate dia) {
+		if(dia == null) {
+			throw new NullPointerException("ERROR: El día de una permanencia no puede ser nulo.");
+		} else {
+			this.dia = dia;
+		}
+	}
+	
+	public abstract int getPuntos();
+
+	public abstract int hashCode();
+
+	public abstract boolean equals(Object obj);
+
+	// Método toString que nos muestra el día y el tramo
+	@Override
+	public String toString() {
+		return "día=" + dia.format(FORMATO_DIA);
+	}
+}
